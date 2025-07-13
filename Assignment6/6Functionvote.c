9dc93bad325ac108ee55bdc6e8a9e863071651aa
mspayalrathod <paaylrathod@gmail.com>
2025-07-14 01:14:59 +0530

    Add files via upload

---
+++ Assignment6/6Functionvote.c
@@ -0,0 +1,19 @@
+//can vote or not
+#include<stdio.h>
+void canVote();
+int main()
+{
+    canvote();
+}
+void canVote()
+
+{
+    int age =15;
+    if(age>18){
+        printf("can vote");
+    }
+    else
+    {
+        printf("cannot vote");
+    }
+}
---
+++ Assignment6/6function triangle area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void TriArea();
+int main()
+{
+    TriArea();
+}
+void TriArea()
+{
+    int base =20, height=30;
+    int area=(base*height)/2;
+    printf("area is %d", area);
+}
